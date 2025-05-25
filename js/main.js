4edf3a32c410830cdcc19cf49573e1abfd47691d
Klukw4 <sinbrom285@gmail.com>
2025-05-25 16:55:32 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 // Можно добавить JS-анимации, слайдеры или интерактивные элементы
 console.log("Сайт Ecokid загружен!");
+// Бургер-меню
+const menuToggle = document.getElementById('menuToggle');
+const navLinks = document.getElementById('navLinks');
+
+if (menuToggle && navLinks) {
+  menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+}
